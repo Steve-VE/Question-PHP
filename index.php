074ace036e77faca01a9fca3d19c92cede8cf846
Leers Mathieu <leers.mathieu@gmail.com>
2018-02-15 09:35:40 +0100

    push structure

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulaire</title>
+</head>
+<body>
+    <div class="page">
+        <header>
+            <!-- Need logo -->
+        </header>
+        <main>
+            <section class="formulaire">
+                <div class="question">
+
+                </div>
+                <div class="question">
+
+                </div>
+                <div class="question">
+
+                </div>
+                <div class="question">
+
+                </div>
+                <div class="question">
+
+                </div> 
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+    
+</body>
+</html>
