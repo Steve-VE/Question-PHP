0a8a74078100687254e759e6d7f7aacdc0fa3e37
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-02-16 16:39:07 +0100

    ajout divers

--- php/scoreboard.php
+++ php/scoreboard.php
@@ -1,10 +1,14 @@
 <?php
     $dataURL = '../database/result.json';
     $scorearray = file_get_contents($dataURL);
-    //scorearray est un tableau concentrant les noms et les scores
-    $scoreboard = arsort($scorearray);
+    $scoreresult = json_decode($scorearray);
+    // $datascore = array($scorearray);
+    //scoreboard est un tableau concentrant les noms et les scores
+    // $scoreboard = arsort($datascore);
     //scoreboard étant le tableau contenant les tableau score/nom mais pas sûr
-    for ($i = 0, $i <= 4, $i++) {
-        print_r($scoreboard[$i]);
-    };
+    // for ($i = 0; $i <= 4; $i++) {
+    //     print_r($scoreboard[$i]);
+    // };
+    var_dump($scorearray);
+    var_dump($scoreresult);
 ?>
