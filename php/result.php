6ea66bbfe27de2d1f490f87ca73c02ad2d18f227
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-02-16 14:07:07 +0100

    database added

---
+++ php/result.php
@@ -0,0 +1,9 @@
+<?php
+$dataURL = '../database/result.json';
+$dataReceived = file_get_contents($dataURL);
+$datafile =  [$_SESSION['user'], $_SESSION['score']];
+$log = json_decode($dataReceived) ;
+$log[] = $datafile;
+$globalResult = json_encode($log);
+file_put_contents($dataURL, $justifResult);
+?>
