0fa5e1f5008f5ae4e7e8c64d07f5ab36e46019ec
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-02-16 09:17:08 +0100

    modif login

--- php/login.php
+++ php/login.php
@@ -1,18 +1,10 @@
-<?php
-if (!isset($_POST['button']) && !isset($_POST['utilisateur'])): ?>
+
 <section class="login">
-  <img src="../assets/images/clippy.jpg" alt="">
+  <?php  if (!isset($_POST['button']) && !isset($_POST['utilisateur'])){
+  echo '<img src="../assets/images/clippy.jpg" alt="">'; }
+  else {echo '<img src="../assets/images/badclippy.jpg" alt="">';} ?>
   <form class="connexion" action="" method="post">
     <label for="utilisateur"></label><input type="text" name="utilisateur" placeholder="Entrez votre nom ici" value="">
     <button type="submit" name="button">connexion</button>
   </form>
 </section>
-<?php elseif (isset($_POST['button']) && $_POST['utilisateur'] == ""): ?>
-  <section class="login">
-    <img src="../assets/images/badclippy.jpg" alt="">
-    <form class="connexion" action="" method="post">
-      <label for="utilisateur"></label><input type="text" name="utilisateur" placeholder="Entrez votre nom ici" value="">
-      <button type="submit" name="button">connexion</button>
-    </form>
-  </section>
-<?php endif; ?>
