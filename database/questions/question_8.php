e4c24e4ee5a9feee6d658fcbbe5feb149e2629f4
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-02-18 18:55:18 +0100

    maj question 9 et 10

--- database/questions/question_8.php
+++ database/questions/question_8.php
@@ -5,8 +5,39 @@ $rnd_line = get_random_value($greetings);
 
 $type = "radio";
 $statement = "A quoi sert la fonction isset() ?";
-$answer = $rnd_name . "='". $rnd_line ."';";
-?>
+$good_answer = "déterminer si une variable est définie et différente de NULL";
+$options = [
+    $good_answer,
+    "vérifier si la variable est initiée",
+    "vérifier si la variable est vide ou est égale à 0",
+];
+shuffle($options);
+define_answer($good_answer);
 
-<label for="Question_9"> <?php echo $statement; ?> </label>
-<input type="radio" name="Question_9"/>
+echo '<p class="label">'. $statement .'</p>';
+echo '<div class="input">';
+// ... et on utilise une boucle pour parcourir le tableau et afficher une à une les différentes options.
+for($j = 0; $j < count($options); $j++){
+
+    $current_option = "Option_" . ($j + 1);
+    // Si l'option actuelle correspond à la bonnse réponse...
+    if( $options[$j] == $good_answer){
+        // ... on indique que cette option est la bonne réponse via la fonction 'define_answer'
+        define_answer( $current_option );
+    }
+
+    echo '<div class="a">';
+    echo '<input type="radio" id="Question_'. $id_question .'" name="Question_'. $id_question .'" value="'. $current_option .'"';
+    if(isset( $_POST['Question_'.$id_question] )
+    && $_POST['Question_'.$id_question] == $current_option ){
+        echo ' checked';
+    }
+    echo '/>';
+    echo '<label for="'. $current_option .'">';
+    echo $options[$j];
+    echo '</label>';
+    echo '</div>';
+}
+echo '</div>';
+
+?>
