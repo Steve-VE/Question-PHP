558929ea3c71f8805a48aee73cd952dfdbd69a0a
Steve-VE <steve.vanessche@gmail.com>
2018-02-18 22:31:26 +0100

    Randomisation des questions 2 et 3

--- database/questions/question_2.php
+++ database/questions/question_2.php
@@ -1,22 +1,22 @@
 <?php
-$rnd_name = get_random_value($variable_names);
-$rnd_line = get_random_value($greetings);
+$rnd_name = get_random_value($variable_text_names);
+$rnd_function_name = get_random_value($function_print_names);
 
 $type = "radio";
 $statement = "Quelle fonction est sans erreurs ?";
 
 // On stocke la bonne réponse dans la variable '$good_answer'...
-$good_answer = "<pre>function afficher(\$texte){<br/>
-                print_r(\$texte);}</pre>";
+$good_answer = "<pre>function ". $rnd_function_name ."(\$". $rnd_name ."){
+                print_r(\$". $rnd_name .");<br/>}</pre>";
 
 $options = [
     $good_answer,
                 
-    "<pre>function afficher(\$texte){<br/>
-    print_r(\$texte)}</pre>",
+    "<pre>function ". $rnd_function_name ."(\$". $rnd_name ."){
+    print_r(\$". $rnd_name .")<br/>}</pre>",
                 
-    "<pre>function afficher(){<br/>
-    print_r(\$texte)}</pre>"
+    "<pre>function ". $rnd_function_name ."(){
+    print_r(\$". $rnd_name .")<br/>}</pre>"
 ];
 echo_question( $type, $statement, $good_answer, $options, "side" );
 ?>
--- database/questions/question_3.php
+++ database/questions/question_3.php
@@ -1,8 +1,19 @@
 <?php
 
 $type = "text";
-$statement = "Quelle fonction permet de mettre un mot ou une phrase en majuscule ?";
-$answer = 'strtoupper';
+$statement = [
+    "Quelle fonction permet de mettre un mot ou une phrase en majuscule ?",
+    "Quelle fonction permet de mettre un mot ou une phrase en minuscule ?",
+    "Quelle fonction permet de savoir si une variable est vide ?",
+    "Quelle fonction permet de calculer la taille d'une chaîne de caractère ?"
+];
+$answer = [
+    'strtoupper',
+    'strtolower',
+    'empty',
+    'strlen'
+];
+$rnd_index = random( 0, count($statement), seed() );
 
-echo_question( $type, $statement, $answer );
+echo_question( $type, $statement[$rnd_index], $answer[$rnd_index] );
 ?>
