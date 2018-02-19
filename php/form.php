8d174fb3b8f05ceb65cf9639f3716ddea5709770
leersmathieu <leers.mathieu@gmail.com>
2018-02-19 10:39:12 +0100

    upgrade result.php

--- php/form.php
+++ php/form.php
@@ -1,7 +1,7 @@
 <?php
 
     $score = 0;
-    $id_question = 5;
+    $id_question = 0;
 
     /* ----------------------------------------------------//
     // Récupération et traitements des réponses utilisateur
