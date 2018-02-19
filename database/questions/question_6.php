4bb59d3f3b364a8ba4ff3999fa5fc640be3bf27c
Steve-VE <steve.vanessche@gmail.com>
2018-02-19 10:37:28 +0100

    Lien debug pour aller sur la page des résultats

--- database/questions/question_6.php
+++ database/questions/question_6.php
@@ -1,16 +1,25 @@
 <?php
 
-$rnd_name = get_random_value($variable_names);
-$rnd_line = get_random_value($greetings);
-
-$type = "text";
+$type = "select";
 $statement = "Prenons une table \$array, si on considère une boucle
 <pre>foreach (\$things as \$key => \$value)</pre>.<br/>
 Que va afficher :<br/><pre>echo \$value</pre> et <br/><pre>echo \$key</pre>";
-$answer = $rnd_name . "='". $rnd_line ."';";
+$answer = [
+    "<pre>echo \$key</pre> va afficher la clé d'un élément du tableau \$things",
+    "<pre>echo \$value</pre> va afficher la valeur contenue dans un élément du tableau \$things"
+];
 
-// echo_question( $type, $statement, $answer );
+$options = [[
+    $answer[0],
+    "<pre>echo \$key</pre> va afficher la valeur contenue dans un élément du tableau \$things",
+    "<pre>echo \$key</pre> va afficher le texte \"key\"",
+    "<pre>echo \$key</pre> va afficher la valeur contenue dans \$value",
+], [
+    $answer[1],
+    "<pre>echo \$value</pre> va afficher la clé d'un élément du tableau \$things",
+    "<pre>echo \$value</pre> va afficher le texte \"value\"",
+    "<pre>echo \$value</pre> va afficher la valeur contenue dans\$key"
+]];
+
+echo_question( $type, $statement, $answer, $options );
 ?>
-
-<label for="Question_7"> <?php echo $statement; ?> </label>
-<input type="text" name="Question_7"/>
--- php/result.php
+++ php/result.php
@@ -1,10 +1,10 @@
 <?php
-$dataURL = '../database/result.json';
+$dataURL = 'database/result.json';
 $dataReceived = file_get_contents($dataURL);
 // $datafile =  [$_SESSION['user'], $_SESSION['score']];
 $datafile = array($_SESSION['user'] => $_SESSION['score']);
 $log = json_decode($dataReceived, 'JSON_FORCE_ARRAY') ;
 $log[] = $datafile;
-$globalResult = json_encode($log, 'JSON_FORCE_OBJECT');
+$globalResult = json_encode($log, JSON_FORCE_OBJECT);
 file_put_contents($dataURL, $globalResult);
 ?>
